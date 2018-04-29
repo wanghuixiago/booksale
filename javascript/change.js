62b42a3e5126b3cb4585dc95e5f9aec9443e402a
Your Name <you@example.com>
2018-04-29 11:36:01 +0800

    booksale

---
+++ javascript/change.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+function change(){
+	document.getElementById("button").value="ÒÑ±£´æ";
+	document.getElementById("button").style.color="#000";
+	document.getElementById("button").style.backgroundColor="grey";
+	
+}
